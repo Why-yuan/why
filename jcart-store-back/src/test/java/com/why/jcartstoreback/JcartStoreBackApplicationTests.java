3a341dc369ac7e6144b329585be630c8a336f944
why <1103220942@qq.com>
2020-02-24 22:42:48 +0800

    first init jcart store back

---
+++ jcart-store-back/src/test/java/com/why/jcartstoreback/JcartStoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.why.jcartstoreback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartStoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
