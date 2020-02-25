46f96168b2d94fdb1ebaacfbd5584f2afcfa5475
why <1103220942@qq.com>
2020-02-25 20:24:47 +0800

    修复一些bug

--- jcart-administration-back/src/main/java/com/why/jcartadministrationback/controller/ImageController.java
+++ jcart-administration-back/src/main/java/com/why/jcartadministrationback/controller/ImageController.java
@@ -2,14 +2,15 @@ package com.why.jcartadministrationback.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/image")
 public class ImageController {
-    @PostMapping("upload")
-    public String upload(MultipartFile image){
+    @PostMapping("/upload")
+    public String upload(@RequestParam MultipartFile image){
         return null;
     }
 }
