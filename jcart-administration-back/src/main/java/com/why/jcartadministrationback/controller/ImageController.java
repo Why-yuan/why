dd68039f53bfe9eb1f2321e65bcce4b503af0324
why <1103220942@qq.com>
2020-02-25 14:15:12 +0800

    add image controller

---
+++ jcart-administration-back/src/main/java/com/why/jcartadministrationback/controller/ImageController.java
@@ -0,0 +1,15 @@
+package com.why.jcartadministrationback.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/image")
+public class ImageController {
+    @PostMapping("upload")
+    public String upload(MultipartFile image){
+        return null;
+    }
+}
